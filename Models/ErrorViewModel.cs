a51f56f331673abe28dfcf0b85c2f422f5f626c7
giselle placencia <placenciag944@gmail.com>
2022-12-19 19:24:15 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace global_Poverty.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
