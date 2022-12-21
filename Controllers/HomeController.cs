a8e966b399f52e161d36b7647caa1c3a13b831bb
giselle placencia <placenciag944@gmail.com>
2022-12-20 21:15:05 -0400

    second commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,15 +13,23 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
-
+    // https://global-economy-analytics.p.rapidapi.com/countries
+    // [HttpGet()]
+    // https://word-bank-world-development-indicators.p.rapidapi.com/charts/line?country=EUU%2CUSA%2CCHN&indicator=NY.GDP.MKTP.CD
     public IActionResult Index()
     {
         var client = new RestClient("https://global-economy-analytics.p.rapidapi.com/countries");
+        // var client = new RestClient("https://word-bank-world-development-indicators.p.rapidapi.com/charts/line?country=EUU%2CUSA%2CCHN&indicator=NY.GDP.MKTP.CD");
+
         var request = new RestRequest();
         request.AddHeader("X-RapidAPI-Key", "c9c7e17bdcmshf50c7817141dd28p1b69e8jsn02ee9a2cf585");
+        // request.AddHeader("X-RapidAPI-Key", "c9c7e17bdcmshf50c7817141dd28p1b69e8jsn02ee9a2cf585");
+
         request.AddHeader("X-RapidAPI-Host", "global-economy-analytics.p.rapidapi.com");
+        // request.AddHeader("X-RapidAPI-Host", "word-bank-world-development-indicators.p.rapidapi.com");
+
         var response = client.Execute(request);
-        Console.WriteLine(response.Content.ToString());
+        // Console.WriteLine(response.Content.ToString());
         return View();
     }
 
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,11 +1,10 @@
-﻿const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'c9c7e17bdcmshf50c7817141dd28p1b69e8jsn02ee9a2cf585',
-        'X-RapidAPI-Host': 'community-worldbank.p.rapidapi.com'
-    }
-};
-fetch('https://community-worldbank.p.rapidapi.com/datacatalog?format=json', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+﻿
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+function countriesLoop() {
+for (let i = 0; i<data.length; i++) {
+    console.log(data[i].cca3)
+}
+}
